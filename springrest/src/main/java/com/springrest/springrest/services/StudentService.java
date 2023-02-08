58a1801c820aa1e1e72737dfbf61a3d0d3610904
Rashi Jaiswal <rashijaiswal1405@gmail.com>
2023-02-08 12:34:20 +0530

    Update StudentService.java

--- springrest/src/main/java/com/springrest/springrest/services/StudentService.java
+++ springrest/src/main/java/com/springrest/springrest/services/StudentService.java
@@ -14,6 +14,6 @@ public interface StudentService {
 
     public Student updateStudent(Student student);
 	
-    public String deleteStudent(long parseLong);
+    public Student deleteStudent(long parseLong);
     
 }
