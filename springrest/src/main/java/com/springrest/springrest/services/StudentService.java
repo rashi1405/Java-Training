720baaa3132707f3f03b3ea17911b3f229c0654f
Rashi Jaiswal <rashijaiswal1405@gmail.com>
2023-02-08 12:22:43 +0530

    Update StudentService.java

--- springrest/src/main/java/com/springrest/springrest/services/StudentService.java
+++ springrest/src/main/java/com/springrest/springrest/services/StudentService.java
@@ -5,12 +5,15 @@ import java.util.List;
 import com.springrest.springrest.entities.Student;
 
 public interface StudentService {
+	
     public List<Student>getStudents() ;
     
     public Student getStudent(long studentId) ;
     
     public Student addStudent(Student student) ;
 
-	public Student updateStudent(Student student);
+    public Student updateStudent(Student student);
+	
+    public String deleteStudent(long parseLong);
     
 }
